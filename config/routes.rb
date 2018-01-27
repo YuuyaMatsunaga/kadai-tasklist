eed8c12a02e1bd995fbac720dd526c3fe303931b
gilgil <workergilgilmode@gmail.com>
2018-01-27 20:51:20 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root to: "tasks#index"
-  resources :tasks
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :tasks, only: [:index, :show, :new, :create]
 end
