30cffb9ca902e30bdea36b9c9cf0ac4a69914176
gilgil <workergilgilmode@gmail.com>
2018-02-10 15:25:29 +0000

    add user model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   
   def index
     if logged_in?
-      @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
+      @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(3)
     end
   end
   
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Task が正常に投稿されました"
@@ -27,9 +27,12 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
   
   def update
+    @task = Task.find(params[:id])
+    
     if @task.update(task_params)
       flash[:success] = "Task は正常に更新されました"
       redirect_to @task
